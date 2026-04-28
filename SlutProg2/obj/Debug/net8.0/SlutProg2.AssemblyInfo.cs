485bbcaf12ccc554bb6d67ba6c9fb11f36faca83
Nilo Tengman <nilo.tengman@elev.ga.ntig.se>
2026-04-28 14:29:36 +0200

    yo2

--- SlutProg2/obj/Debug/net8.0/SlutProg2.AssemblyInfo.cs
+++ SlutProg2/obj/Debug/net8.0/SlutProg2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SlutProg2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f1fa8493297296fef65cb9ade410d8f9496d51d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SlutProg2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SlutProg2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
